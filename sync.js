d32da0e5cbc994fa850a6885465e15af92a2c0c8
diego-medina-sgc <diego.medina@stgeorges.edu.ar>
2026-06-09 17:25:35 -0300

    Update sync.js

--- sync.js
+++ sync.js
@@ -55,7 +55,7 @@
     try {
       fetch(URL, {
         method: 'POST',
-        mode: 'no-cors',
+        mode: 'cors',
         headers: { 'Content-Type': 'text/plain;charset=utf-8' },
         body: JSON.stringify({ action: 'save', token: token, rev: rev, content: content })
       });
