f0c52128f5d5a1ba2262fedf42f441e288f2f5e2
diego-medina-sgc <diego.medina@stgeorges.edu.ar>
2026-06-09 14:43:14 -0300

    Update data.js

--- data.js
+++ data.js
@@ -375,7 +375,7 @@ window.TGN_DEFAULTS = {
 const _ov = function (k, def) { return (window.TGNStore ? TGNStore.get(k, def) : def); };
 window.TGN = {
   I18N: I18N, DEPTS: DEPTS, EXTENSIONS: EXTENSIONS, MENUS: MENUS, PAGES: PAGES, SOCIAL: SOCIAL,
-  config: { driveApiKey: '', remoteUrl: 'https://script.google.com/macros/s/AKfycbyRU34yCF8fjj-2GqePLxGODiTlm9_4u1AqlNHCKM0ehtoTedUAkQ82RX5Pyo_adSA/exec' },
+  config: { driveApiKey: '', remoteUrl: 'https://script.google.com/macros/s/AKfycbweYv9vKruF54ln44O6LlEDRbYaasrBYy6FS-7na2JzprLq3VzEQaIKgG2mQxVjl0mV/exec' },
   NEWS: _ov('news', NEWS),
   BIRTHDAYS: _ov('birthdays', BIRTHDAYS),
   VACANCIES: _ov('vacancies', VACANCIES),
